2f496bbccf8341de746356f43013164c01a3215c
VarunRaj7 <rvarunraj1@gmail.com>
2020-06-17 23:27:52 -0400

    added the update-todo feature configurations

--- backend/src/lambda/http/updateTodo.ts
+++ backend/src/lambda/http/updateTodo.ts
@@ -1,13 +1,30 @@
-// import 'source-map-support/register'
+import 'source-map-support/register'
 
-// import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyHandler,
+  APIGatewayProxyResult
+} from 'aws-lambda'
 
-// import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
+import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
 
-// export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-//   const todoId = event.pathParameters.todoId
-//   const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
+import { createLogger } from '../../utils/logger'
 
-//   // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
-//   return undefined
-// }
+import { createTodo } from '../../businessLogic/Todo'
+
+export const handler: APIGatewayProxyHandler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+  const todoId = event.pathParameters.todoId
+  const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
+
+  const updateItemResp = await 
+  return {
+    statusCode: updateItemResp.status,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: JSON.stringify(deleteItemResp.results)
+  }
+}
